d34c2aa94385ccdc8bc7042a115d16a88c331b66
GameInPeaceStudio <128132776+GameInPeaceStudio@users.noreply.github.com>
2024-01-30 22:30:32 +0200

    Reload and max bullet
    Added some new mechanics
    
    reload + reload cooldown +maxbullet

--- DarkerSide/Assets/C#/Guns.cs
+++ DarkerSide/Assets/C#/Guns.cs
@@ -1,3 +1,4 @@
+using System.Collections;
 using UnityEngine;
 using UnityEngine.InputSystem; 
 public class Guns : MonoBehaviour
@@ -8,6 +9,10 @@ public class Guns : MonoBehaviour
     public float bulletSpeed;
     public float fireRate = 0.5f;
     public float lastFireTime;
+    public int maxBullet = 10;  
+    private int currentBullet = 0;
+    public int reloadCooldownTime=3;
+
     public GameObject bulletPrefab;
     public GameObject BulletSpawnPosition;
 
@@ -18,6 +23,12 @@ public class Guns : MonoBehaviour
     void Start()
     {
         Bulletrb = GetComponent<Rigidbody2D>();
+        currentBullet = maxBullet;
+        Debug.Log("Bullet: " + currentBullet);
+    }
+    void Update()
+    {
+        TakeInput();
     }
 
     // Update is called once per frame
@@ -75,16 +86,39 @@ public class Guns : MonoBehaviour
 
         if (Input.GetButton("Fire1") && Time.time > lastFireTime + fireRate)
         {
-            SpawnBullet();
-            lastFireTime = Time.time;
+            if (currentBullet > 0)
+            {
+                SpawnBullet();
+                lastFireTime = Time.time;
+            }
+            else
+            {
+               
+                Debug.Log("Reloading");
+
+                StartCoroutine(ReloadCooldown());
+
+            }
+            
         }
-        /*
-        if (Input.GetMouseButton(0)&&Time.time>lastFireTime+fireRate)
+       
+    }
+
+    void TakeInput()
+    {
+        //Taking Input for reloud 
+        if(Input.GetKeyDown(KeyCode.R))
         {
-           SpawnBullet();
-           lastFireTime = Time.time;
+            StartCoroutine(ReloadCooldown());
         }
-        */
+    }
+    IEnumerator ReloadCooldown()
+    {
+        yield return new WaitForSeconds(reloadCooldownTime);
+
+        currentBullet = maxBullet;
+
+        Debug.Log("Reloaded");
     }
 
     void SpawnBullet()
@@ -94,7 +128,10 @@ public class Guns : MonoBehaviour
 
         GameObject bullet = Instantiate(bulletPrefab, spawnPosition, transform.rotation);
 
-        // Opsiyonel: Mermi objesine hız eklemek istiyorsanız Rigidbody2D bileşenini kullanabilirsiniz
+    
         bullet.GetComponent<Rigidbody2D>().velocity = transform.up * bulletSpeed;
+
+        currentBullet--;
+        Debug.Log("Bullet: " + currentBullet);
     }
 }
