33cc76514fc3b7e8f8adf0766965de1c21d80fa2
GameInPeaceStudio <128132776+GameInPeaceStudio@users.noreply.github.com>
2024-02-07 18:12:03 +0200

    yep! I did it
    Weapon pick Up is working now.

---
+++ DarkerSide/Assets/C#/weapons/WeaponPickup.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WeaponPickup : MonoBehaviour
+{
+
+    public GameObject[] weapons;
+    public GameObject PickThisWeapon;
+
+    //public int WeaponNumber;
+
+
+    void Start()
+    {
+        weapons[0].SetActive(true);
+    }
+
+    
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+
+        if(collision.CompareTag("Player"))
+        {
+            Debug.Log("weapon is here");
+
+            foreach(GameObject weapon in weapons)
+            {
+                weapon.SetActive(false);
+            }
+
+            PickThisWeapon.SetActive(true);
+        }
+        
+
+    }
+}
